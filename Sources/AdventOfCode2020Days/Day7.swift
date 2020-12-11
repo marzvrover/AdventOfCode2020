df44cdb7e49555ea11ead160b916645cab76f22e
Marz Rover <matthew@dooley.xyz>
2020-12-11 06:34:23 -0500

    Add Day 7

--- Sources/AdventOfCode2020Days/Day7.swift
+++ Sources/AdventOfCode2020Days/Day7.swift
@@ -12,11 +12,91 @@ public struct Day7: Day {
 
     public func resultString(input: String) -> String {
         return resultStringHelper(input: input,
-                                  parts: [self.numberOfBagsEventuallyContainGold],
+                                  parts: [Self.numberOfBagsEventuallyContainShinyGold,
+                                          Self.countBagsInShinyGold],
                                   errorMessage: "no bags could be found")
     }
+
+    static func numberOfBagsEventuallyContainShinyGold(input: String) -> Int? {
+        let rules = Self.readRules(input: input)
+        var bags = 0
+        for (bag, _) in rules {
+            if Self.canContainColor(haystack: bag, needle: "shiny gold", rules: rules) {
+                bags += 1
+            }
+        }
+        return bags != 0 ? bags : nil
+    }
     
-    func numberOfBagsEventuallyContainGold(input: String) -> Int? {
-        return nil
+    static func countBagsInShinyGold(input: String) -> Int? {
+        let rules = Self.readRules(input: input)
+        return Self.countBagsIn(color: "shiny gold", rules: rules)
+    }
+
+    static func readRules(input: String) -> [String:[String:Int]] {
+        var rules: [String:[String:Int]] = [:]
+
+        input.enumerateLines { line, _ in
+            let explodedContains = line.components(separatedBy: " bags contain ")
+            let key = explodedContains[0]
+            var bagRules: [String: Int] = [:]
+            var nextToParse = explodedContains[1]
+            scanLineRules: while true {
+                guard nextToParse.prefix(2) != "no" else {
+                    break
+                }
+                let num = Int(nextToParse.prefix(1))!
+                nextToParse = String(nextToParse.dropFirst(2)) // "# "
+                let explodedParses = nextToParse.components(separatedBy: " bag")
+                bagRules[String(explodedParses[0])] = num
+                parseRule: while true {
+                    if nextToParse.prefix(1) != "." && nextToParse.prefix(2) != ", " {
+                        nextToParse = String(nextToParse.dropFirst(1))
+                    } else if nextToParse.prefix(2) == ", " {
+                        nextToParse = String(nextToParse.dropFirst(2)) // ", "
+                        break parseRule
+                    } else { // reached . therefore end of line
+                        break scanLineRules
+                    }
+                }
+            }
+            rules[key] = bagRules
+        }
+
+        return rules
+    }
+
+    static func canContainColor(haystack: String, needle: String, rules: [String:[String:Int]]) -> Bool {
+        guard rules[haystack] != nil else {
+            return false
+        }
+        guard !rules[haystack]!.isEmpty else {
+            return false
+        }
+        guard !rules[haystack]!.contains(where: { key, _ in
+            return key == needle
+        }) else {
+            return true
+        }
+        for (rule, _) in rules[haystack]! {
+            if Self.canContainColor(haystack: rule, needle: needle, rules: rules) {
+                return true
+            }
+        }
+        return false
+    }
+
+    static func countBagsIn(color: String, rules: [String:[String:Int]]) -> Int {
+        guard rules[color] != nil else {
+            return 0
+        }
+        guard !rules[color]!.isEmpty else {
+            return 0
+        }
+        var count = 0
+        for (bag, number) in rules[color]! {
+            count += number + (number * Self.countBagsIn(color: bag, rules: rules))
+        }
+        return count
     }
 }
---
+++ Tests/AdventOfCode2020DaysTests/Day7Tests.swift
@@ -0,0 +1,76 @@
+import XCTest
+@testable import AdventOfCode2020Days
+
+final class Day7Tests: XCTestCase {
+    let input = """
+    light red bags contain 1 bright white bag, 2 muted yellow bags.
+    dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+    bright white bags contain 1 shiny gold bag.
+    muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+    shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+    dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+    vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+    faded blue bags contain no other bags.
+    dotted black bags contain no other bags.
+    """
+    let inputValue = [
+        "light red": [
+            "bright white": 1,
+            "muted yellow": 2,
+        ],
+        "dark orange": [
+            "bright white": 3,
+            "muted yellow": 4,
+        ],
+        "bright white": [
+            "shiny gold": 1,
+        ],
+        "muted yellow": [
+            "shiny gold": 2,
+            "faded blue": 9,
+        ],
+        "shiny gold": [
+            "dark olive": 1,
+            "vibrant plum": 2,
+        ],
+        "dark olive": [
+            "faded blue": 3,
+            "dotted black": 4,
+        ],
+        "vibrant plum": [
+            "faded blue": 5,
+            "dotted black": 6,
+        ],
+        "faded blue": [:],
+        "dotted black": [:],
+    ]
+    let input2 = """
+    shiny gold bags contain 2 dark red bags.
+    dark red bags contain 2 dark orange bags.
+    dark orange bags contain 2 dark yellow bags.
+    dark yellow bags contain 2 dark green bags.
+    dark green bags contain 2 dark blue bags.
+    dark blue bags contain 2 dark violet bags.
+    dark violet bags contain no other bags.
+    """
+
+    func testReadRules() {
+        XCTAssertEqual(Day7.readRules(input: input), inputValue)
+    }
+
+    func testCanContain() {
+        XCTAssertTrue(Day7.canContainColor(haystack: "dark orange", needle: "shiny gold", rules: inputValue))
+        XCTAssertTrue(Day7.canContainColor(haystack: "dark orange", needle: "dotted black", rules: inputValue))
+        XCTAssertFalse(Day7.canContainColor(haystack: "dark orange", needle: "shiny black", rules: inputValue))
+    }
+
+    func testCountBagsInShinyGold() {
+        XCTAssertEqual(Day7.countBagsInShinyGold(input: input)!, 32)
+        XCTAssertEqual(Day7.countBagsInShinyGold(input: input2)!, 126)
+    }
+
+    static var allTests = [
+        ("testReadRules", testReadRules),
+        ("testCanContain", testCanContain),
+    ]
+}
