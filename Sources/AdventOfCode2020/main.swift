8ffa6fbdb97565f06fbb604101680945e721f5c4
Marz Rover <matthew@dooley.xyz>
2020-12-12 02:56:37 -0500

    Add swift-argument-parser to select days to run

--- Sources/AdventOfCode2020/main.swift
+++ Sources/AdventOfCode2020/main.swift
@@ -5,12 +5,11 @@
 //  Created by Marz Rover on 12/6/20.
 //
 
-import Foundation
 import AdventOfCode2020Days
+import ArgumentParser
+import Foundation
 
-print("Welcome to Marz's Advent of Code 2020".green)
-
-let days: [(day: Day, number: String, resource: String)] = [
+let allDays: [(day: Day, number: String, resource: String)] = [
     (day: Day1(), number: "one", resource: "day1"),
     (day: Day2(), number: "two", resource: "day2"),
     (day: Day3(), number: "three", resource: "day3"),
@@ -23,8 +22,40 @@ let days: [(day: Day, number: String, resource: String)] = [
     (day: Day10(), number: "ten", resource: "day10"),
 ]
 
-for (day, number, resource) in days {
-    let url = Bundle.module.url(forResource: resource, withExtension: "txt")!
-    let file = try! String(contentsOf: url)
-    print("Day \(number):\n\(day.resultString(input: file))")
+enum Days: String, EnumerableFlag {
+    case Day1, Day2, Day3, Day4, Day5, Day6, Day7, Day8, Day9, Day10
 }
+
+struct AdventOfCode2020: ParsableCommand {
+    @Flag(help: "Days you would like to run.")
+    var days: [Days] = []
+
+    mutating func run() {
+        print("Welcome to Marz's Advent of Code 2020".green)
+
+        guard !days.isEmpty else {
+            return runAllDays()
+        }
+
+        for day in days.sorted(by: {
+            Int($0.rawValue.components(separatedBy: CharacterSet.decimalDigits.inverted).joined())!
+                < Int($1.rawValue.components(separatedBy: CharacterSet.decimalDigits.inverted).joined())!
+        }) {
+            let index = Int(day.rawValue.components(separatedBy: CharacterSet.decimalDigits.inverted).joined())! - 1
+            let challenge = allDays[index]
+            let url = Bundle.module.url(forResource: challenge.resource, withExtension: "txt")!
+            let file = try! String(contentsOf: url)
+            print("Day \(challenge.number):\n\(challenge.day.resultString(input: file))")
+        }
+    }
+    
+    func runAllDays() {
+        for (day, number, resource) in allDays {
+            let url = Bundle.module.url(forResource: resource, withExtension: "txt")!
+            let file = try! String(contentsOf: url)
+            print("Day \(number):\n\(day.resultString(input: file))")
+        }
+    }
+}
+
+AdventOfCode2020.main()
